9850f4a45761ffb7e0ebadadac9f84213009da53
Nicolas van de Walle <nicolas@walle.be>
2024-05-19 08:41:20 +0000

    refactor: Add PIIForm component for autofill information

---
+++ src/components/PIIForm.js
@@ -0,0 +1,44 @@
+const PIIForm = ({ ssn, setSsn, name, setName, address, setAddress }) => {
+  return (
+    <>
+      <h2 className="text-2xl mb-4">
+        Autofill info{" "}
+        <span className="text-red-500 my-0 text-xs">(Optional)</span>
+      </h2>
+
+      <div className="my-4">
+        <label htmlFor="purchasePrice">Social Security Number (SSN)</label>
+        <input
+          name="ssn"
+          id="ssn"
+          className="w-full bg-slate-100 rounded h-12 px-2 my-2"
+          placeholder="##.##.##-###.##"
+          onChange={(e) => setSsn(e.target.value)}
+          value={ssn}
+        />
+      </div>
+      <div className="my-4">
+        <label htmlFor="purchaseDate">Full name</label>
+        <input
+          name="name"
+          id="name"
+          className={`w-full bg-slate-100 rounded h-12 px-2 my-2`}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div className="my-4">
+        <label htmlFor="purchaseDate">Address</label>
+        <input
+          name="address"
+          id="address"
+          className={`w-full bg-slate-100 rounded h-12 px-2 my-2`}
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+      </div>
+    </>
+  );
+};
+
+export default PIIForm;
--- src/steps/Email.js
+++ src/steps/Email.js
@@ -1,62 +1,33 @@
-import translations from "../helper/translations";
-import AttachmentImage from "../assets/attach-file.png";
+import { useState } from "react";
+import EmailView from "../components/EmailView";
+import PIIForm from "../components/PIIForm";
 
 const Email = ({ language, purchaseDate, tob }) => {
+  const [ssn, setSsn] = useState("");
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+
   return (
-    <div className="md:w-2/3  rounded-2xl shadow-md mx-auto flex-col ">
-      <div className="bg-gray-100 rounded-t-xl py-2 px-4">
-        <strong>New email</strong>
+    <div className="flex mt-4 md:space-x-12 flex-col md:flex-row">
+      <div className="md:w-1/3">
+        <PIIForm
+          ssn={ssn}
+          setSsn={setSsn}
+          name={name}
+          setName={setName}
+          address={address}
+          setAddress={setAddress}
+        />
       </div>
-      <div className="py-1 px-4 border-b-2">From: You</div>
-      <div className="py-1 px-4 border-b-2">
-        To: <strong>CPIC.TAXDIV@minfin.fed.be</strong>
-      </div>
-      <div className="py-1 px-4 border-b-2">
-        Subject: <strong>{translations.emailSubject[language]}</strong>
-      </div>
-      <div className="py-2 px-4">
-        {translations.dear[language]},
-        <br />
-        <br />
-        {translations.emailIntro[language]}
-        {"  "}
-        {translations.months[purchaseDate.getMonth() + 1][language]}{" "}
-        {purchaseDate.getFullYear()}
-        {language === "nl" && " "}
-        {translations.emailIntroContinued[language]}.
-        <br />
-        {translations.ssn[language]}:{" "}
-        <span className="text-red-600">[Your Social Security Number]</span>
-        <br />
-        {translations.name[language]}:{" "}
-        <span className="text-red-600">[Your full name]</span>
-        <br />
-        {translations.address[language]}:{" "}
-        <span className="text-red-600">[Your address]</span>
-        <br />
-        {translations.amount[language]}: {tob}€
-        <br />
-        {translations.communication[language]}: TOB{" "}
-        <span className="text-red-600">[Your Social Security Number]</span>{" "}
-        {purchaseDate.getMonth() + 1}/{purchaseDate.getFullYear()} (
-        {translations.seeAttached[language]})
-        <br />
-        <br />
-        <span className="text-red-600">[Your signature]</span>
-        <br />
-      </div>
-      <div className="py-2 px-4 mt-4">
-        Attachments:
-        <ul className="flex flex-row my-2">
-          <li className="bg-slate-100 px-4 py-2 rounded-md me-2 shadow hover:bg-slate-200 flex flex-row items-center">
-            <img src={AttachmentImage} alt="attachment" className="h-6 me-2" />
-            TOB declaration
-          </li>
-          <li className="bg-slate-100 px-4 py-2 rounded-md me-2 shadow hover:bg-slate-200 flex flex-row items-center">
-            <img src={AttachmentImage} alt="attachment" className="h-6 me-2" />
-            <span>Proof of payment</span>
-          </li>
-        </ul>
+      <div className="md:w-2/3">
+        <EmailView
+          language={language}
+          purchaseDate={purchaseDate}
+          tob={tob}
+          ssn={ssn}
+          address={address}
+          name={name}
+        />
       </div>
     </div>
   );
